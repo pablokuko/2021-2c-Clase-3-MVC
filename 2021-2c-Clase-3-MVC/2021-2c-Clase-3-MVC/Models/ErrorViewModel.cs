1afc9435c715575e103853d09079caf279445fb9
Pablo Nicolas Sanchez <pablokuko@gmail.com>
2021-09-08 19:59:06 -0300

    agregando proyecto mvc inicial y gitignore

---
+++ 2021-2c-Clase-3-MVC/2021-2c-Clase-3-MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace _2021_2c_Clase_3_MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
