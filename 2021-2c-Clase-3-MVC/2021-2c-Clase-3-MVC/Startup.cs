984aa414ac84a8d0504aa348a6011a1fe3574476
Pablo Nicolas Sanchez <pablokuko@gmail.com>
2021-09-08 20:54:43 -0300

    agregando edicion de vistas razor en runtime (para no tener q reiniciar la aplicacion)

--- 2021-2c-Clase-3-MVC/2021-2c-Clase-3-MVC/Startup.cs
+++ 2021-2c-Clase-3-MVC/2021-2c-Clase-3-MVC/Startup.cs
@@ -23,7 +23,7 @@ namespace _2021_2c_Clase_3_MVC
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllersWithViews();
+            services.AddControllersWithViews().AddRazorRuntimeCompilation();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
