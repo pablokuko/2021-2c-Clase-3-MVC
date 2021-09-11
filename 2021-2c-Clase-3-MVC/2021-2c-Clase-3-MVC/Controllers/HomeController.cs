3054425802340a12d1bea8c894f898fb6a45dbc7
Gaston Perez <gastonperez-97@hotmail.com>
2021-09-11 15:18:02 -0300

    formatted code

--- 2021-2c-Clase-3-MVC/2021-2c-Clase-3-MVC/Controllers/HomeController.cs
+++ 2021-2c-Clase-3-MVC/2021-2c-Clase-3-MVC/Controllers/HomeController.cs
@@ -7,31 +7,25 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace _2021_2c_Clase_3_MVC.Controllers
-{
-    public class HomeController : Controller
-    {
-        private readonly ILogger<HomeController> _logger;
+namespace _2021_2c_Clase_3_MVC.Controllers {
+	public class HomeController : Controller {
+		private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
+		public HomeController(ILogger<HomeController> logger) {
+			_logger = logger;
+		}
 
-        public IActionResult Index()
-        {
-            return View();
-        }
+		public IActionResult Index() {
+			return View();
+		}
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
+		public IActionResult Privacy() {
+			return View();
+		}
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
-    }
+		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+		public IActionResult Error() {
+			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+		}
+	}
 }
--- 2021-2c-Clase-3-MVC/2021-2c-Clase-3-MVC/Controllers/ZodiacoController.cs
+++ 2021-2c-Clase-3-MVC/2021-2c-Clase-3-MVC/Controllers/ZodiacoController.cs
@@ -7,89 +7,70 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace _2021_2c_Clase_3_MVC.Controllers
-{
-    public class ZodiacoController : Controller
-    {
-        // GET: ZodiacoController
-        public IActionResult Index()
-        {
-            return View(SignosServicio.ObtenerTodosCronologicamente());
-        }
+namespace _2021_2c_Clase_3_MVC.Controllers {
+	public class ZodiacoController : Controller {
+		// GET: ZodiacoController
+		public IActionResult Index() {
+			return View(SignosServicio.ObtenerTodosCronologicamente());
+		}
 
-        [HttpGet]
-        public IActionResult Nuevo()
-        {
-            return View();
-        }
+		[HttpGet]
+		public IActionResult Nuevo() {
+			return View();
+		}
 
-        [HttpPost]
-        public IActionResult Nuevo(Signo signo)
-        {
-            try
-            {
-                SignosServicio.Agregar(signo);
-            }
-            catch (SignoExistenteException ex)
-            {
-                ViewBag.Mensaje = ex.Message;
-                return View(signo);
-            }
+		[HttpPost]
+		public IActionResult Nuevo(Signo signo) {
+			try {
+				SignosServicio.Agregar(signo);
+			} catch (SignoExistenteException ex) {
+				ViewBag.Mensaje = ex.Message;
+				return View(signo);
+			}
 
-            return Redirect("Index");
-        }
+			return Redirect("Index");
+		}
 
-        [HttpGet]
-        public IActionResult NuevoConBindingAutomaticoInd()
-        {
-            return View();
-        }
+		[HttpGet]
+		public IActionResult NuevoConBindingAutomaticoInd() {
+			return View();
+		}
 
-        [HttpPost]
-        public IActionResult NuevoConBindingAutomaticoInd(string Nombre, DateTime FechaInicio, DateTime FechaFin, string Url)
-        {
-            Signo signo = new Signo() { Nombre = Nombre, FechaFin = FechaFin, FechaInicio = FechaInicio, Url = Url };
-            try
-            {
-                SignosServicio.Agregar(signo);
-            }
-            catch (SignoExistenteException ex)
-            {
-                ViewBag.Mensaje = ex.Message;
-                return View(signo);
-            }
+		[HttpPost]
+		public IActionResult NuevoConBindingAutomaticoInd(string Nombre, DateTime FechaInicio, DateTime FechaFin, string Url) {
+			Signo signo = new Signo() { Nombre = Nombre, FechaFin = FechaFin, FechaInicio = FechaInicio, Url = Url };
+			try {
+				SignosServicio.Agregar(signo);
+			} catch (SignoExistenteException ex) {
+				ViewBag.Mensaje = ex.Message;
+				return View(signo);
+			}
 
-            return Redirect("Index");
-        }
+			return Redirect("Index");
+		}
 
 
-        [HttpGet]
-        public IActionResult NuevoBindingManual()
-        {
-            return View();
-        }
+		[HttpGet]
+		public IActionResult NuevoBindingManual() {
+			return View();
+		}
 
-        [HttpPost]
-        public IActionResult NuevoBindingManualPost()
-        {
-            Signo signo = new Signo() 
-            {
-                Nombre = Request.Form["Nombre"], 
-                FechaFin = DateTime.Parse(Request.Form["FechaFin"]), 
-                FechaInicio = DateTime.Parse(Request.Form["FechaInicio"]), 
-                Url = Request.Form["Url"]
-            };
-            try
-            {
-                SignosServicio.Agregar(signo);
-            }
-            catch (SignoExistenteException ex)
-            {
-                ViewBag.Mensaje = ex.Message;
-                return View(signo);
-            }
+		[HttpPost]
+		public IActionResult NuevoBindingManualPost() {
+			Signo signo = new Signo() {
+				Nombre = Request.Form["Nombre"],
+				FechaFin = DateTime.Parse(Request.Form["FechaFin"]),
+				FechaInicio = DateTime.Parse(Request.Form["FechaInicio"]),
+				Url = Request.Form["Url"]
+			};
+			try {
+				SignosServicio.Agregar(signo);
+			} catch (SignoExistenteException ex) {
+				ViewBag.Mensaje = ex.Message;
+				return View(signo);
+			}
 
-            return Redirect("Index");
-        }
-    }
+			return Redirect("Index");
+		}
+	}
 }
